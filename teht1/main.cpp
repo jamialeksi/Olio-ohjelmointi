0fcb754ee2794e9aba9b1637ec100ad5739b9e0a
Jami Pietila <jami0809@windowslive.com>
2025-01-12 18:03:10 +0200

    tehtävä 1 valmis

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
+int game(int maxnum);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int arvausten_lkm = 0;
+    int maxnum = 40;
+    arvausten_lkm = game(maxnum);
+    cout << "arvauksia: "
+         << arvausten_lkm
+         << endl
+        ;
     return 0;
+}
+
+int game(int maxnum) {
+    int arvausten_lkm = 0;
+    int satunnaisluku = 0;
+    int arvaus = 0;
+
+    srand(time(NULL));
+    satunnaisluku = rand() % maxnum;
+
+    while (true) {
+
+        cout << "Anna luku? "
+             << endl;
+        cin >> arvaus;
+        arvausten_lkm++;
+
+        if (arvaus == satunnaisluku) {
+            cout << "Oikein!"
+                 << endl;
+            break;
+        }
+
+        if (arvaus < satunnaisluku) {
+            cout << "Luku on suurempi"
+                 << endl;
+        }
+        if (arvaus > satunnaisluku) {
+            cout << "luku on pienempi"
+                 << endl;
+        }
+
+    }
+    return arvausten_lkm;
 }
